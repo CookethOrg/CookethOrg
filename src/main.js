d8708ad3c0fa8c05768402fe920fa80f493921a0
Mrutyunjay Lodhi <2004lodhi@gmail.com>
2025-06-06 12:20:37 +0530

    nav alomost compleated

--- src/main.js
+++ src/main.js
@@ -1,20 +1,10 @@
-import './style.css'
-import './header.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
+console.log("JavaScript loaded");
 
-// Load header
-fetch('/CookethOrg/partial_hf/header.html')
-  .then(res => res.text())
-  .then(data => {
-    document.getElementById('header').innerHTML = data;
-  });
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
 
-// Load footer
-fetch('./partial_hf/footer.html')
-  .then(res => res.text())
-  .then(data => {
-    document.getElementById('footer').innerHTML = data;
-  });
+hamburger.addEventListener('click', () => {
+  console.log("Hamburger clicked");
+  navLinks.classList.toggle('active');
+});
