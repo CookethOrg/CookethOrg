ca175ff6d8cb3938ff173f1314d53487fd977870
Mrutyunjay Lodhi <2004lodhi@gmail.com>
2025-06-06 19:10:13 +0530

    nav done footer done

--- src/main.js
+++ src/main.js
@@ -5,6 +5,6 @@ const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
 
 hamburger.addEventListener('click', () => {
-  console.log("Hamburger clicked");
   navLinks.classList.toggle('active');
+  
 });
