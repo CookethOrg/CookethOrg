97a3921df11e5a2f5a9fc1acaac3bc17ad0a53ad
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2025-08-14 22:54:31 +0530

    Nav Footer Link Fix - 4

--- components/script.js
+++ components/script.js
@@ -23,6 +23,6 @@ function highlightActiveNav() {
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Starting Load...")
-  loadComponent("navbar", "navbar.html", highlightActiveNav);
-  loadComponent("footer", "footer.html");
+  loadComponent("navbar", "components/navbar.html", highlightActiveNav);
+  loadComponent("footer", "components/footer.html");
 });
