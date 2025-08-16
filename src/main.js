7ace27a53fdeb610939dc0861f3d461ceab7beed
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2025-08-16 12:05:09 +0530

    Swiper.js integration in Carousel + Team Section Update

--- src/main.js
+++ src/main.js
@@ -18,3 +18,26 @@ document.querySelectorAll('.faqQues').forEach(question => {
     const icon = question.querySelector('span');
   });
 });
+
+// Carousel JS
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
