8571ae1dc57986706cea2c45f36a3bf7cd35d440
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2025-08-16 23:30:38 +0530

    Contact Form Functionality Added

--- src/main.js
+++ src/main.js
@@ -18,3 +18,45 @@ document.querySelectorAll('.faqQues').forEach(question => {
     const icon = question.querySelector('span');
   });
 });
+
+// Contact Form Submissions
+const form = document.getElementById("contactForm");
+const submitBtn = document.getElementById("submitBtn");
+const btnText = document.getElementById("btnText");
+const btnLoader = document.getElementById("btnLoader");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  btnText.textContent = "";
+  btnLoader.style.display = "flex";
+  submitBtn.disabled = true;
+
+  try {
+    const data = new FormData(form);
+    const object = Object.fromEntries(data);
+    const json = JSON.stringify(object);
+
+    const response = await fetch("https://submit-form.com/0L6QO6wvG", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: json,
+    });
+
+    if (response.ok) {
+      console.log("Thank you! Your message has been sent.");
+      form.reset();
+    } else {
+      alert("Something went wrong, please try again.");
+      console.log("Something went wrong, please try again.");
+    }
+  } catch (error) {
+    console.log("Network error. Try again later.");
+    console.log(error)
+  } finally {
+    btnText.textContent = "Submit";
+    btnLoader.style.display = "none";
+    submitBtn.disabled = false;
+    form.reset();
+  }
+});
